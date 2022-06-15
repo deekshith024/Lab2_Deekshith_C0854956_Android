23a4bfca78977a808aa222b0db43e018237c9800
Deekshith024 <deekshithreddy0001@gmail.com>
2022-06-15 10:56:15 -0400

    display row 1

--- app/src/main/java/com/example/lab2_deekshith_c0854956_android/MainActivity.java
+++ app/src/main/java/com/example/lab2_deekshith_c0854956_android/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.lab2_deekshith_c0854956_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
 
+    CustomAdaptor customAdaptor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
         add_button = findViewById(R.id.add_button);
-        add_button.setOnClickListener(new View.OnClickListener() {
+        add_button.setOnClickListener((view) -> {
 
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ProductActivity.class);
-                startActivity(intent);
+                    Intent intent = new Intent(MainActivity.this, ProductActivity.class);
+                    startActivity(intent);
 
-            }
-        });
+            });
 
         myDB = new DbHelper (MainActivity.this);
         product_id = new ArrayList<>();
@@ -43,8 +45,39 @@ public class MainActivity extends AppCompatActivity {
         product_price =  new ArrayList<>();
 
 
-
-
-
+        storeDataInArrays();
+        customAdaptor = new CustomAdaptor(MainActivity.this, product_id, product_name, product_description, product_price);
+        recyclerView.setAdapter(customAdaptor);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
+
+    void storeDataInArrays() {
+
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0) {
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
+        }else{
+            while (cursor.moveToNext()) {
+                product_id.add(cursor.getString(0));
+                product_name.add(cursor.getString(1));
+                product_description.add(cursor.getString(2));
+                product_price.add(cursor.getString(3));
+
+            }
+        }
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
